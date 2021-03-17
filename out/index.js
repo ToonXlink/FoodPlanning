d6286b2a54011fbf9de8f9203df2d29a5cdedd2d
ToonXlink <philip.winter1@gmx.de>
2021-03-17 20:26:38 +0100

    preparations

--- out/index.js
+++ out/index.js
@@ -4,14 +4,44 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var mariadb_1 = __importDefault(require("mariadb"));
 var app = express_1.default();
-var port = 3000;
+var port = 3001;
+var pool = mariadb_1.default.createPool({
+    host: 'localhost',
+    user: 'FoodPlanningUser',
+    password: 'Shaymin',
+    database: 'FoodPlanning',
+    connectionLimit: 5
+});
+pool.getConnection()
+    .then(function (conn) {
+    /*      conn.query("SELECT 1 as val")
+            .then((rows) => {
+              console.log(rows); //[ {val: 1}, meta: ... ]
+              //Table must have been created before
+              // " CREATE TABLE myTable (id int, val varchar(255)) "
+              return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+            })
+            .then((res) => {
+              console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+              conn.end();
+            })
+            .catch(err => {
+              //handle error
+              console.log(err);
+              conn.end();
+            })
+    */
+}).catch(function (err) {
+    console.log(err);
+    //not connected
+});
 app.use(function (req, res, next) {
     console.log(Date.now() + " [" + req.method + "] " + req.path);
     next();
 });
 app.get('/', function (req, res) {
-    console.log('moin');
     res.send('Hello World');
 });
 app.listen(port, function () {
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,51 @@
 import express from 'express';
+import mariadb from 'mariadb';
 
-let app = express();
-let port = 3000;
+const app = express();
+const port = 3001;
+const pool = mariadb.createPool({
+    host: 'localhost',
+    user: 'FoodPlanningUser',
+    password: 'Shaymin',
+    database: 'FoodPlanning',
+    connectionLimit: 5
+})
 
-app.use((req, res, next) =>{
+pool.getConnection()
+    .then(conn => {
+    
+/*      conn.query("SELECT 1 as val")
+        .then((rows) => {
+          console.log(rows); //[ {val: 1}, meta: ... ]
+          //Table must have been created before 
+          // " CREATE TABLE myTable (id int, val varchar(255)) "
+          return conn.query("INSERT INTO myTable value (?, ?)", [1, "mariadb"]);
+        })
+        .then((res) => {
+          console.log(res); // { affectedRows: 1, insertId: 1, warningStatus: 0 }
+          conn.end();
+        })
+        .catch(err => {
+          //handle error
+          console.log(err); 
+          conn.end();
+        })
+*/ 
+    }).catch(err => {
+        console.log(err);
+      //not connected
+    });
+
+
+
+app.use((req, res, next) => {
     console.log(`${Date.now()} [${req.method}] ${req.path}`)
     next();
 })
 
-app.get('/',(req, res) => {
-    console.log('moin')
+app.get('/', (req, res) => {
     res.send('Hello World');
+
 })
 
 app.listen(port, () => {
